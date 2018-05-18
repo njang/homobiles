af37699ae3775ee562f8dd655792c7a295d30270
njang <neo.w.jang@gmail.com>
2018-05-18 08:00:00 -0500

    Differentiate login/logout views

--- src/App.js
+++ src/App.js
@@ -93,30 +93,42 @@ class App extends Component {
             {this.state.user ? <button onClick={this.logout}>Log Out</button> : <button onClick={this.login}>Log In</button> }
           </div>
         </header>
-        <div className='container'>
-          <section className='add-item'>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" name="username" placeholder="What's your name?"  onChange={this.handleChange} value={this.state.username} />
-              <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
-              <button>Add Item</button>
-            </form>
-          </section>
-          <section className='display-item'>
-            <div className='wrapper'>
-              <ul>
-                {this.state.items.map((item) => {
-                  return (
-                    <li key={item.id}>
-                      <h3>{item.title}</h3>
-                      <p>brought by: {item.user}</p>
-                      <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                    </li>
-                  )
-                })}
-              </ul>
+        {this.state.user ?
+          <div>
+            <div className='user-profile'>
+              <img src={this.state.user.photoURL} />
             </div>
-          </section>
-        </div>
+          </div>
+        //   <div className='container'>
+        //     <section className='add-item'>
+        //       <form onSubmit={this.handleSubmit}>
+        //         <input type="text" name="username" placeholder="What's your name?"  onChange={this.handleChange} value={this.state.username} />
+        //         <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
+        //         <button>Add Item</button>
+        //       </form>
+        //     </section>
+        //     <section className='display-item'>
+        //       <div className='wrapper'>
+        //         <ul>
+        //           {this.state.items.map((item) => {
+        //             return (
+        //               <li key={item.id}>
+        //                 <h3>{item.title}</h3>
+        //                 <p>brought by: {item.user}</p>
+        //                 <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
+        //               </li>
+        //             )
+        //           })}
+        //         </ul>
+        //       </div>
+        //     </section>
+        //   </div>
+        // </div>
+        :
+          <div className='wrapper'>
+            <p>You must be logged in to see the potluck list and submit to it.</p>
+          </div>
+        }
       </div>
     );
   }
