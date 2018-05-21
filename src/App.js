804196ecca868fcf1c3f2f51ca76b3d531339897
njang <neo.w.jang@gmail.com>
2018-05-21 08:03:00 -0500

    Show delete button only for the owner of the item

--- src/App.js
+++ src/App.js
@@ -114,8 +114,9 @@ class App extends Component {
                       return (
                         <li key={item.id}>
                           <h3>{item.title}</h3>
-                          <p>brought by: {item.user}
-                          {item.user === this.state.user.displayName || item.user === this.state.user.email ? <button onClick={() => this.removeItem(item.id)}>Remove Item</button> : null}
+                          <p>
+                            brought by: {item.user}
+                            {item.user === this.state.user.displayName || item.user === this.state.user.email ? <button onClick={() => this.removeItem(item.id)}>Remove Item</button> : null}
                           </p>
                         </li>
                       )
