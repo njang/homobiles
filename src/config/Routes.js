c33d02c2bacc927de0515792378d0362ad5854f3
njang <neo.w.jang@gmail.com>
2018-05-28 10:26:00 -0500

    Add Routes

---
+++ src/config/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+// import ChildrenContainer from '../containers/ChildrenContainer'
+// import AddChildForm from '../components/AddChild'
+
+export default (
+    <Switch>
+        {/* <Route exact path='/' component={ ChildrenContainer } />
+        <Route exact path='/children' component={ ChildrenContainer } />
+        <Route exact path='/child/add' component={ AddChildForm } />
+        <Route path='/child/:id' component={ ChildContainer } /> */}
+    </Switch>
+)
+
